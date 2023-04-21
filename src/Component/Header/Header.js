2ef78e1517daf82d14e4e6573213fd93102a2206
Mstchadnee <116938357+Mstchadnee@users.noreply.github.com>
2023-04-21 15:25:37 +0600

    first commit

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='d-flex justify-content-between align-items-center'>
+            <div>
+                <span className=''>Knowledge Cafe</span>
+            </div>
+            <div>
+                <nav className='d-flex gap-3'>
+                  <a href='#'>Home</a>
+                  <a href='#'>About</a>
+                </nav>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
